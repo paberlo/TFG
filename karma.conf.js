aa49675b387eab3ac962cfa6ac7d8bbf7a8da30a
PabloB <pablo.bermejo@uclm.es>
2020-02-19 13:40:10 +0100

    chromeheadless

--- karma.conf.js
+++ karma.conf.js
@@ -36,12 +36,12 @@ module.exports = function(config) {
     // Level of logging, can be: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    /* browsers: ['ChromeHeadless'], */
-    browsers: ['Chrome-headless'],
+    browsers: ['ChromeHeadless'],
+    //browsers: ['Chrome-headless'],
     customLaunchers: {
-      'Chrome-headless': {
-        base: 'Chrome',
-        flags: ['--headless', '--remote-debugging-port=9222', '--no-sandbox']
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     },
     singleRun: false,
